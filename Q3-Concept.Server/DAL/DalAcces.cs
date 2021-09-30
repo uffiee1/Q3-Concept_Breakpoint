522f2f8733f312460d1e0f1fe826c4c104dd4c01
Thisoban Mahalingam <I419727@fhict.nl>
2021-09-30 09:50:43 +0200

    made connection with db and first setup

--- Q3-Concept.Server/DAL/DalAcces.cs
+++ Q3-Concept.Server/DAL/DalAcces.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 
 namespace DAL
 {
     public class DalAcces
     {
+        public MySqlConnection conn;
+        public DalAcces()
+        {
+            conn = new MySqlConnection("server=studmysql01.fhict.local;Uid=dbi419727;Database=dbi419727;pwd=test;");
+        }
     }
 }
