5c2a451b27be1f9b94943c65701708582c4abec1
Thisoban Mahalingam <I419727@fhict.nl>
2021-09-30 10:04:34 +0200

    first connection with first method for getting data from database

--- Q3-Concept.Server/DAL/Components.cs
+++ Q3-Concept.Server/DAL/Components.cs
@@ -1,14 +1,56 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DAL
 {
     public class Components
     {
-      //id naam omschrijving 
+        //id naam omschrijving 
+        private readonly DalAcces _dalaccess = new DalAcces();
+
+        public List<DataModel.ComponentDataModel> GetAllComponents()
+        {
+            List<DataModel.ComponentDataModel> componentlist = new List<DataModel.ComponentDataModel>();
+            string query = "SELECT * FROM product";
+
+            _dalaccess.conn.Open();
+            MySqlCommand command = new MySqlCommand(query, _dalaccess.conn);
+            MySqlDataReader reader = command.ExecuteReader();
+            try
+            {
+                //read through all the data
+                while (reader.Read())
+                {
+                    //create productlist
+                    //DataModel.ComponentDataModel listofcomponents = new DataModel.ComponentDataModel
+                    //{
+                    //    ID = reader.GetInt32("IdUser"),
+                    //    Name = reader.GetString("Name"),
+                    //    Description = reader.GetString(""),
+                    //    meters = reader.GetString(""),
+
+                    //};
+                    //// save uitlening to the list
+                    //orderList.Add(order);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("kan de query niet uitvoeren! LOL");
+            }
+            finally
+            {
+                _dalaccess.conn.Close();
+            }
+
+            return componentlist;
+            throw new NotImplementedException();
+        }
 
     }
 }
