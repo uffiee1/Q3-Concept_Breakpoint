6cd4dcc6fff87ac195a75813bce441690a93385a
Thisoban Mahalingam <I419727@fhict.nl>
2021-10-12 10:33:34 +0200

    changes tweaked

--- Q3-Concept.Server/DALUnitTest/DALTests/ComponentTests.cs
+++ Q3-Concept.Server/DALUnitTest/DALTests/ComponentTests.cs
@@ -11,15 +11,17 @@ namespace DALUnitTest
         [TestMethod]
         public void GetGoodComponent()
         {
+            //act 
             ComponentDataModel component = new ComponentDataModel()
             {
                 ID = 276,
                 Name = "41301",
                 Description="Deksel 173 4V"
             };
-
+            //arrange
             ComponentDataModel testcomponent = _dalComponents.GetComponent(276);
-            //Model.ComponentDataModel component = new Model.ComponentDataModel() { }
+
+            //assert
             Assert.IsNotNull(testcomponent);
             Assert.AreEqual(component.Description, testcomponent.Description);
         }
