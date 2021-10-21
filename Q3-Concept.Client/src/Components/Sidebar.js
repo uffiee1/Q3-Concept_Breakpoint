53c47c42db80203499939d23a159f479b042294a
BartHagoort <bart.hagoort@hotmail.nl>
2021-10-21 09:18:14 +0200

    changes to sidebar

--- Q3-Concept.Client/src/Components/Sidebar.js
+++ Q3-Concept.Client/src/Components/Sidebar.js
@@ -1,13 +1,7 @@
 import '../css/Sidebar.css';
 
 import React, { useState, useEffect } from 'react';
-
-import { Link } from 'react-router-dom';
 import SidebarMachineList from './SidebarMachineList';
-
-import Home from '../pages/Home';
-
-import jQuery from 'jquery';
 
 function Sidebar({productionlinearray = machinelist}) {
 
@@ -21,31 +15,31 @@ function Sidebar({productionlinearray = machinelist}) {
 }
 
   //default dummy array
-  const machinelist = [
-    {
-      id: 1,
-      machineName: "Machine 11",
-      path: '/',
-      status: 'active',
-    },
-    {
-      id: 2,
-      machineName: "Machine 12",
-      path: '/',
-      status: 'inactive'
-    },
-    {
-      id: 3,
-      machineName: "Machine 13",
-      path: '/',
-      status: 'maintenance'
-    },
-    {
-      id: 4,
-      machineName: "Machine 14",
-      path: '/',
-      status: 'requires-maintance'
-    }
-  ];
+  // const machinelist = [
+  //   {
+  //     id: 1,
+  //     machineName: "Machine 11",
+  //     path: '/',
+  //     status: 'active',
+  //   },
+  //   {
+  //     id: 2,
+  //     machineName: "Machine 12",
+  //     path: '/',
+  //     status: 'inactive'
+  //   },
+  //   {
+  //     id: 3,
+  //     machineName: "Machine 13",
+  //     path: '/',
+  //     status: 'maintenance'
+  //   },
+  //   {
+  //     id: 4,
+  //     machineName: "Machine 14",
+  //     path: '/',
+  //     status: 'requires-maintance'
+  //   }
+  // ];
 
 export default Sidebar
