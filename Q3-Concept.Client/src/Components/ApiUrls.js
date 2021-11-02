8fc97e5bc52515e392060bdaa29ea0b1cc73347f
BartHagoort <bart.hagoort@hotmail.nl>
2021-11-02 15:34:30 +0100

    Fixed frontend

--- Q3-Concept.Client/src/Components/ApiUrls.js
+++ Q3-Concept.Client/src/Components/ApiUrls.js
@@ -1,4 +1,4 @@
 export const Variables = {
-    GetAllProductionLinesUrl: "https://localhost:5000/ProductionLine/ProductionLineDetails?StartTime=2020-09-26&EndTime=2020-09-27",
-    GetAllComponentsUrl: "https://localhost:5000/Component/ComponentsAll",
+    GetAllProductionLinesUrl: "http://localhost:5000/ProductionLine/ProductionLineDetails?StartTime=2020-09-26&EndTime=2020-09-27",
+    GetAllComponentsUrl: "http://localhost:5000/Component/ComponentsAll",
 };
