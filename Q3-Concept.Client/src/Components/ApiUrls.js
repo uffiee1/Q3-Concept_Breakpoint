2b834bf64ad774a6025069412eb399e90cfeffc4
thisoban <thisoban_1995@hotmail.com>
2021-11-02 14:56:47 +0100

    component page made showing information form api

--- Q3-Concept.Client/src/Components/ApiUrls.js
+++ Q3-Concept.Client/src/Components/ApiUrls.js
@@ -1,3 +1,4 @@
 export const Variables = {
     GetAllProductionLinesUrl: "https://localhost:44321/ProductionLine/ProductionLineDetails?StartTime=2020-09-26&EndTime=2020-09-27",
+    GetAllComponentsUrl: "https://localhost:44321/Component/ComponentsAll",
 };
