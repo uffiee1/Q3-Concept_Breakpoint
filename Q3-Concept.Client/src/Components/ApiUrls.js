55e2159dbd1c9c0a06f2556083c5dfed1e5f4702
Ufuk Angay <u.angay@gmail.com>
2021-11-02 15:20:46 +0100

    Backend Fixed

--- Q3-Concept.Client/src/Components/ApiUrls.js
+++ Q3-Concept.Client/src/Components/ApiUrls.js
@@ -1,4 +1,4 @@
 export const Variables = {
-    GetAllProductionLinesUrl: "https://localhost:44321/ProductionLine/ProductionLineDetails?StartTime=2020-09-26&EndTime=2020-09-27",
-    GetAllComponentsUrl: "https://localhost:44321/Component/ComponentsAll",
+    GetAllProductionLinesUrl: "https://localhost:5000/ProductionLine/ProductionLineDetails?StartTime=2020-09-26&EndTime=2020-09-27",
+    GetAllComponentsUrl: "https://localhost:5000/Component/ComponentsAll",
 };
