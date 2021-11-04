6493f9c928a0038b24d17a95b342886ee31356c5
NickVanGerwen <47970324+NickVanGerwen@users.noreply.github.com>
2021-11-04 14:12:42 +0100

    Bar graph x-as

--- Q3-Concept.Client/src/Components/BarItem.js
+++ Q3-Concept.Client/src/Components/BarItem.js
@@ -20,13 +20,22 @@ function GetCollor(status) {
     //if you change the colors make sure to change them in statuscollors.scss as well
 }
 
+
 function BarItem({ statusarray }) {
+    var s = [0, 4, 8, 12, 16, 20, 24].reverse();
+    var x = 0;
+
+    const tickFormatter = d => {
+        var y = s[x]
+        x = x + 1;
+        return y;
+    };
+
     return (
-        <XYPlot width={300} height={100} stackBy="x">
+        <XYPlot class="Bar" width={276} height={100} stackBy="x">
             <VerticalGridLines />
             <HorizontalGridLines />
-            <XAxis />
-
+            <XAxis tickTotal={[7]} tickFormat={tickFormatter} xDomain={[0, 30]} />
             {statusarray.map((item) => (
                 <HorizontalBarSeries data={[{ y: 1, x: item.duration }]} stroke='black' color={GetCollor(item.description)} />
             ))}
