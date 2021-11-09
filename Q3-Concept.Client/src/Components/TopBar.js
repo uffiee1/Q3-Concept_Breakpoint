f4d74c79ecf693a433093bf7c99247245ceb907e
Ufuk Angay <u.angay@gmail.com>
2021-11-09 09:43:02 +0100

    Update Topbar Title

--- Q3-Concept.Client/src/Components/TopBar.js
+++ Q3-Concept.Client/src/Components/TopBar.js
@@ -3,7 +3,7 @@ import '../css/TopBar.css'
 function TopBar() {
     return (
         <div className="topbar">
-            <h1 className="title" > productionline</h1>
+            <h1 className="title" > Machine Monitoring Data</h1>
         </div>
     );
 }
