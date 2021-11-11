d3b42a0ea01d4fec4a07a7c58d6b30e35e66b2a9
NickVanGerwen <47970324+NickVanGerwen@users.noreply.github.com>
2021-11-11 13:13:55 +0100

    component cards -> table

--- Q3-Concept.Client/src/Components/ComponentList.js
+++ Q3-Concept.Client/src/Components/ComponentList.js
@@ -1,16 +1,36 @@
 import "../css/ComponentCard.scss"
-
 import ComponentCard from "./ComponentCard"
+
 
 function ComponentList({ components }) {
     return (
-        <div class="row">
-            {components.map((component) => (
-                <div className="column" >
-                    <ComponentCard key={component.id} ComponentName={component.name} ComponentDescription={component.description} ComponentActions={component.actions} />
-                </div>
-            ))}
-        </div>
+        //component table //npm install react-bootstrap bootstrap@5.1.3
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Naam</th>
+                    <th>Beschrijving</th>
+                    <th>Handelingen</th>
+                </tr>
+            </thead>
+            <tbody>
+                {components.map(component => (
+                    <tr key={component.id}>
+                        <th>{component.name}</th>
+                        <th>{component.description}</th>
+                        <th>{component.actions}</th>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        //component cards
+        // <div class="row">
+        //     {components.map((component) => (
+        //         <div className="column" >
+        //             <ComponentCard key={component.id} ComponentName={component.name} ComponentDescription={component.description} ComponentActions={component.actions} />
+        //         </div>
+        //     ))}
+        // </div>
     )
 }
 
