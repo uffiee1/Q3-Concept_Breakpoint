ef0aaee6e737423caced427f056a2a47cd7bd34c
Bingley,Lucas L.P <lucas.bingley@student.fontys.nl>
2021-11-11 11:47:37 +0100

    searchInputBar - does not filter

--- Q3-Concept.Client/src/Components/ComponentList.js
+++ Q3-Concept.Client/src/Components/ComponentList.js
@@ -1,10 +1,28 @@
 import "../css/ComponentCard.scss"
+import { useState, useEffect } from "react"
 
 import ComponentCard from "./ComponentCard"
 
 function ComponentList({ components }) {
+
+    const [filteredComponents, setFilteredComponents] = useState([])
+
+
+
+    function filterComponentsOnChange(event){
+        const searchInput = event.target.value;
+        console.log(searchInput)
+        setFilteredComponents(components.filter( c => c.name.includes(searchInput)))
+        console.log(filteredComponents)
+        return 
+    }
     return (
+        
         <div class="row">
+            <div className="Searchbar">
+                <label htmlFor="search">Search by name</label>
+                <input id="searchField" type="text" onChange={filterComponentsOnChange}></input>
+            </div>
             {components.map((component) => (
                 <div className="column" >
                     <ComponentCard key={component.id} ComponentName={component.name} ComponentDescription={component.description} />
