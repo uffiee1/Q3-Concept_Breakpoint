044b1022170ba289edbca69463b4e179504c6086
Ufuk Angay <u.angay@gmail.com>
2021-11-16 15:01:23 +0100

    Start Fixing

--- Q3-Concept.Client/src/Components/ComponentList.js
+++ Q3-Concept.Client/src/Components/ComponentList.js
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react"
 
 import ComponentCard from "./ComponentCard"
 
-
 function ComponentList({ components }) {
 
     const [filteredComponents, setFilteredComponents] = useState([])
@@ -69,34 +68,25 @@ function ComponentList({ components }) {
                     </div>
                 ))
             }
-        </div >
-        //component table //npm install react-bootstrap bootstrap@5.1.3
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Naam</th>
-                    <th>Beschrijving</th>
-                    <th>Handelingen</th>
-                </tr>
-            </thead>
-            <tbody>
-                {components.map(component => (
-                    <tr key={component.id}>
-                        <th>{component.name}</th>
-                        <th>{component.description}</th>
-                        <th>{component.actions}</th>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Naam</th>
+                        <th>Beschrijving</th>
+                        <th>Handelingen</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
-        //component cards
-        // <div class="row">
-        //     {components.map((component) => (
-        //         <div className="column" >
-        //             <ComponentCard key={component.id} ComponentName={component.name} ComponentDescription={component.description} ComponentActions={component.actions} />
-        //         </div>
-        //     ))}
-        // </div>
+                </thead>
+                <tbody>
+                    {components.map(component => (
+                        <tr key={component.id}>
+                            <th>{component.name}</th>
+                            <th>{component.description}</th>
+                            <th>{component.actions}</th>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div >
     )
 }
 
