fd1d33cbe08b29cd3e601dcf1a8f5a924d472166
Ufuk Angay <u.angay@gmail.com>
2021-11-16 12:22:07 +0100

    Added SortingByName in ComponentsList

--- Q3-Concept.Client/src/Components/ComponentList.js
+++ Q3-Concept.Client/src/Components/ComponentList.js
@@ -1,63 +1,95 @@
 import "../css/ComponentCard.scss"
-import { useState, useEffect } from "react"
+
+import { useEffect, useState } from "react"
 
 import ComponentCard from "./ComponentCard"
 
 function ComponentList({ components }) {
 
+    const [Compare, setCompare] = useState(false)
+    const [sortedComponents, setSortedComponents] = useState([])
     const [filteredComponents, setFilteredComponents] = useState([])
     const [notFound, setNotFound] = useState(false)
-    
+
 
     let searchInput = ""
 
-    function filterComponentsByName(component){
-        if(component.name.includes(searchInput)){
+    function filterComponentsByName(component) {
+        if (component.name.includes(searchInput)) {
             return true
         }
         return false
     }
 
-    async function filterComponentsOnChange(event){
+    async function filterComponentsOnChange(event) {
         searchInput = event.target.value;
         console.log(searchInput)
         let filtered = components.filter(filterComponentsByName)
-        if(filtered.length === 0){
+        if (filtered.length === 0) {
             setNotFound(true)
         }
-        else{setNotFound(false);}
+        else { setNotFound(false); }
 
         setFilteredComponents(filtered)
-        return 
+        return
     }
+
+    function compare(a, b) {
+        if (a.name < b.name) {
+            return -1
+        }
+        if (a.name > b.name) {
+            return 1
+        }
+        return 0;
+    }
+
+    function sortByAscending() {
+        let sorted = components.sort(compare)
+        setSortedComponents(sorted)
+        setCompare(true)
+    }
+
+    useEffect(() => {
+        console.log(sortedComponents)
+    }, [sortedComponents])
 
     useEffect(() => {
         console.log(filteredComponents)
     }, [filteredComponents])
 
-    useEffect(() =>{
+    useEffect(() => {
         console.log(notFound)
     }, [notFound])
-    
+
     return (
-        
         <div class="row">
             <div className="Searchbar">
                 <label className="searchLabel" htmlFor="search">Search by name:</label>
                 <input className="searchField" type="text" onChange={filterComponentsOnChange}></input>
                 {notFound === true ? <label>No results found</label> : null}
             </div>
-            {filteredComponents.length >= 1 ? filteredComponents.map((component) => (
-                <div className="column" >
-                <ComponentCard key={component.id} ComponentName={component.name} ComponentDescription={component.description} />
-            </div>
-            )): components.map((component) => (
-                <div className="column" >
-                    <ComponentCard key={component.id} ComponentName={component.name} ComponentDescription={component.description} />
-                </div>
-            ))}
-            
-        </div>
+
+            <input type="submit" onClick={sortByAscending} value="Sorteer op naam" >
+            </input>
+
+            {
+                filteredComponents.length >= 1 ? filteredComponents.map((component) => (
+                    <div className="column" >
+                        <ComponentCard key={component.id} ComponentName={component.name} ComponentDescription={component.description} />
+                    </div>
+                )) : sortedComponents.length >= 1 ? sortedComponents.map((component) => (
+                    <div className="column" >
+                        <ComponentCard key={component.id} ComponentName={component.name} ComponentDescription={component.description} />
+                    </div>
+                )) : components.map((component) => (
+                    <div className="column" >
+                        <ComponentCard key={component.id} ComponentName={component.name} ComponentDescription={component.description} />
+                    </div>
+                ))
+            }
+
+        </div >
     )
 }
 
