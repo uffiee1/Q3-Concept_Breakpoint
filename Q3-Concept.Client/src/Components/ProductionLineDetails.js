490c9c85d7db809c7f2dde93dddb154720e7aa98
NickVanGerwen <47970324+NickVanGerwen@users.noreply.github.com>
2021-11-18 13:04:00 +0100

    Productionline details popup

--- Q3-Concept.Client/src/Components/ProductionLineDetails.js
+++ Q3-Concept.Client/src/Components/ProductionLineDetails.js
@@ -1,40 +1,32 @@
 import '../css/ProductionLineDetails.css';
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Variables } from '../Components/ApiUrls';
 
 
 function ProductionLineDetails({ productionline }) {
-    const [componentHistory, setComponentHistory] = useState([])
-
-    async function getComponentHistory() {
-        try {
-            const apirequest = await axios.get(Variables.GetAllComponentsUrl);
-
-            console.log(apirequest.data);
-            return apirequest.data;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function GetComponentHistory() {
-        setComponentHistory(await getComponentHistory());
-        return;
-    }
-
-    useEffect(() => {
-        GetComponentHistory()
-    })
-
-
     return (
         <div className="productionLinePopup">
-            <h1>Machine Info</h1>
-            <div>
-                <h3>{productionline.id}</h3>
-                <h5>{productionline.name}</h5>
-            </div>
+            <h1>{productionline.name}</h1>
+            <h5>Actuele componenten</h5>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Naam</th>
+                        <th>Beschrijving</th>
+                        <th>Start Datum</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {productionline.components.length >= 1 ? productionline.components.map(component => (
+                        <tr key={component.id}>
+                            <th>{component.name}</th>
+                            <th>{component.description}</th>
+                            <th>{component.startDate}</th>
+                        </tr>
+                    )) :
+                        <div />
+                    }
+                </tbody>
+            </table>
+            <h5>Component geschiedenis</h5>
             <table class="table">
                 <thead>
                     <tr>
@@ -45,19 +37,22 @@ function ProductionLineDetails({ productionline }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {componentHistory.length >= 1 ? componentHistory.map(component => (
-                        <tr key={component.id}>
-                            <th>{component.name}</th>
-                            <th>{component.description}</th>
-                            <th>{component.startdate}</th>
-                            <th>{component.enddate}</th>
-                        </tr>
-                    )) :
+                    {productionline.componentHistory.length >= 1 ? productionline.componentHistory.map(component => (
+                        component.endDate === '0001-01-01T00:00:00' ?
+                            <div />
+                            :
+                            <tr key={component.id}>
+                                <th>{component.name}</th>
+                                <th>{component.description}</th>
+                                <th>{component.startDate}</th>
+                                <th>{component.endDate}</th>
+                            </tr>
+                    ))
+                        :
                         <div />
                     }
                 </tbody>
             </table>
-
         </div>
     )
 }
